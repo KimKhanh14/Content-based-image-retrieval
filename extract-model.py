c6dcb48bbfb1167c8adb5daf5c7e12dc3dfed054
Nguyễn Anh Tuấn <19127614@student.hcmus.edu.vn>
2022-02-14 21:44:05 +0700

    retrieval image

--- extract-model.py
+++ extract-model.py
@@ -1,5 +1,4 @@
 import os
-import math
 import pickle
 import numpy as np
 
@@ -11,7 +10,6 @@ from keras.preprocessing import image
 from keras.applications.vgg16 import VGG16, preprocess_input
 from keras.models import Model
 
-# Ham tao model
 def get_extract_model():
     vgg16_model = VGG16(weights="imagenet")
     extract_model = Model(inputs=vgg16_model.inputs, outputs = vgg16_model.get_layer("fc1").output)
@@ -37,43 +35,31 @@ def extract_vector(model, image_path):
     vector = vector / np.linalg.norm(vector)
     return vector
 
-# Dinh nghia anh can tim kiem
-search_image = "tiger.jpg"
+
+
+
+# Dinh nghia thu muc data
+
+data_folder = "dataset"
 
 # Khoi tao model
 model = get_extract_model()
 
-# Trich dac trung anh search
-search_vector = extract_vector(model, search_image)
+vectors = []
+paths = []
 
-# Load 4700 vector tu vectors.pkl ra bien
-vectors = pickle.load(open("vectors.pkl","rb"))
-paths = pickle.load(open("paths.pkl","rb"))
+for image_path in os.listdir(data_folder):
+    # Noi full path
+    image_path_full = os.path.join(data_folder, image_path)
+    # Trich dac trung
+    image_vector = extract_vector(model,image_path_full)
+    # Add dac trung va full path vao list
+    vectors.append(image_vector)
+    paths.append(image_path_full)
 
-# Tinh khoang cach tu search_vector den tat ca cac vector
-distance = np.linalg.norm(vectors - search_vector, axis=1)
+# save vao file
+vector_file = "vectors.pkl"
+path_file = "paths.pkl"
 
-# Sap xep va lay ra K vector co khoang cach ngan nhat
-K = 16
-ids = np.argsort(distance)[:K]
-
-# Tao oputput
-nearest_image = [(paths[id], distance[id]) for id in ids]
-
-# Ve len man hinh cac anh gan nhat do
-import matplotlib.pyplot as plt
-
-axes = []
-grid_size = int(math.sqrt(K))
-fig = plt.figure(figsize=(10,5))
-
-
-for id in range(K):
-    draw_image = nearest_image[id]
-    axes.append(fig.add_subplot(grid_size, grid_size, id+1))
-
-    axes[-1].set_title(draw_image[1])
-    plt.imshow(Image.open(draw_image[0]))
-
-fig.tight_layout()
-plt.show()
+pickle.dump(vectors, open(vector_file, "wb"))
+pickle.dump(paths, open(path_file, "wb"))
